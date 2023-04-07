50900cbd82d8d83b700c058586705f3507bcc2bf
Navid Shokri <navid.pdp11@gmail.com>
2023-04-07 13:30:18 +0330

    add jakarta package test path variable adjust driver class name

--- todo/src/main/java/com/example/todo/controllers/TodoController.java
+++ todo/src/main/java/com/example/todo/controllers/TodoController.java
@@ -12,16 +12,29 @@ import com.example.todo.models.TodoItem;
 import com.example.todo.pojos.requests.CreateTodoItemRequest;
 import com.example.todo.pojos.responses.CommandResponse;
 
+import jakarta.persistence.Id;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 @RestController
 public class TodoController {
     @Autowired
     private TodoRepository todoRepository; 
     
-    @PostMapping
+    @PostMapping(value = "/")
     public ResponseEntity createTodoItem(@RequestBody CreateTodoItemRequest request){
 
         var todo = new TodoItem(request.getTitle(), request.getDescription()); 
         todoRepository.save(todo);
         return new ResponseEntity(new CommandResponse<Integer>(todo.getId()), HttpStatusCode.valueOf(200)); 
     }
+
+    @GetMapping(value="/{id}")
+    public String getTodoItam(@PathVariable Integer id) {
+        return String.format("this is %s", id); 
+    }
+    
 }
